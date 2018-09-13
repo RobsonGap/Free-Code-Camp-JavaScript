ec5e90dfa9c2a94b38cf7601bade9a64c9a061d9
Robson Alves <robsonmie@hotmail.com>
2018-09-13 11:31:39 -0300

    Compound Assignment With Augmented Subtraction 2
    Basic JavaScript: Compound Assignment With Augmented Subtraction
    
    Like the += operator, -= subtracts a number from a variable.
    
    myVar = myVar - 5;
    
    will subtract 5 from myVar. This can be rewritten as:
    
    myVar -= 5;
    
    Convert the assignments for a, b, and c to use the -= operator.
    
    JavaScript básico: atribuição composta com subtração aumentada
    
    Como o operador + =, - = subtrai um número de uma variável.
    
    myVar = myVar - 5;
    
    subtrairá 5 do myVar. Isso pode ser reescrito como:
    
    myVar - = 5;
    
    Converta as atribuições de a, b e c para usar o operador - =.
    outra forma de fazer o mesmo codigo

---
+++ Compound Assignment With Augmented Subtraction2.js
@@ -0,0 +1,15 @@
+var a = 11;
+var b = 9;
+var c = 3;
+
+// Only modify code below this line
+
+a = a -= 6;
+b = b -= 15;
+c = c -= 1;
+
+//posso fazer assim tambem que esta certo
+a -= 6;
+b -= 15;
+c -= 1;
+// Um codigo mais puro mais simples
