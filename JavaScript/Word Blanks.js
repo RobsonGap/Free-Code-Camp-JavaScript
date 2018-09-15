e4f0af71c771ead6a7ba7e565b90d88c547423c2
Robson Alves <robsonmie@hotmail.com>
2018-09-15 15:02:05 -0300

    Word Blanks
    Basic JavaScript: Word Blanks
    
    We will now use our knowledge of strings to build a "Mad Libs" style word game we're calling "Word Blanks". You will create an (optionally humorous) "Fill in the Blanks" style sentence.
    
    In a "Mad Libs" game, you are provided sentences with some missing words, like nouns, verbs, adjectives and adverbs. You then fill in the missing pieces with words of your choice in a way that the completed sentence makes sense.
    
    Consider this sentence - "It was really ____, and we ____ ourselves ____". This sentence has three missing pieces- an adjective, a verb and an adverb, and we can add words of our choice to complete it. We can then assign the completed sentence to a variable as follows:
    
        var sentence = "It was really" + "hot" + ", and we" + "laughed" + "ourselves" + "silly.";
    
    In this challenge, we provide you with a noun, a verb, an adjective and an adverb. You need to form a complete sentence using words of your choice, along with the words we provide.
    
    You will need to use the string concatenation operator + to build a new string, using the provided variables: myNoun, myAdjective, myVerb, and myAdverb. You will then assign the formed string to the result variable.
    
    You will also need to account for spaces in your string, so that the final sentence has spaces between all the words. The result should be a complete sentence.
    
    
    JavaScript básico: espaços em branco do Word
    
    Agora usaremos nosso conhecimento de strings para criar um jogo de palavras estilo "Mad Libs" que estamos chamando de "Word Blanks". Você criará uma frase "Preencha os espaços em branco" (opcionalmente engraçada).
    
    Em um jogo "Mad Libs", você recebe frases com algumas palavras ausentes, como substantivos, verbos, adjetivos e advérbios. Você então preenche as partes que faltam com palavras de sua escolha de uma forma que a sentença completa faça sentido.
    
    Considere esta frase - "Foi realmente ____, e nós ____ nós mesmos ____". Esta frase tem três partes faltando - um adjetivo, um verbo e um advérbio, e podemos adicionar palavras de nossa escolha para completá-lo. Podemos então atribuir a frase completa a uma variável da seguinte forma:
    
        var sentença = "Foi realmente" + "quente" + ", e nós" + "rimos" + "nós mesmos" + "bobos.";
    
    Neste desafio, fornecemos a você um substantivo, um verbo, um adjetivo e um advérbio. Você precisa formar uma frase completa usando palavras de sua escolha, juntamente com as palavras que fornecemos.
    
    Você precisará usar o operador de concatenação de strings + para construir uma nova string, usando as variáveis ​​fornecidas: myNoun, myAdjective, myVerb e myAdverb. Você então atribuirá a string formada à variável de resultado.
    
    Você também precisará considerar espaços em sua string, para que a frase final tenha espaços entre todas as palavras. O resultado deve ser uma frase completa.

---
+++ JavaScript/Word Blanks.js
@@ -0,0 +1,11 @@
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  // Your code below this line
+  var result = "";
+
+  // Your code above this line
+  return result+= "My "+myAdjective+" "+myNoun+" "+myVerb+" very "+myAdverb+".";
+  
+}
+
+// Change the words here to test your function
+wordBlanks("dog", "big", "ran", "quickly");
