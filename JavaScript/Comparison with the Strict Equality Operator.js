1932d67567cdc3f81d11e2080a8af8a22c538832
Robson Alves <robsonmie@hotmail.com>
2018-10-02 22:48:16 -0300

    Comparison with the Strict Equality Operator
    Basic JavaScript: Comparison with the Strict Equality Operator
    
    Strict equality (===) is the counterpart to the equality operator (==). However, unlike the equality operator, which attempts to convert both values being compared to a common type, the strict equality operator does not perform a type conversion.
    
    If the values being compared have different types, they are considered unequal, and the strict equality operator will return false.
    
    Examples
    
        3 === 3 // true
        3 === '3' // false
    
    In the second example, 3 is a Number type and '3' is a String type.
    
    Use the strict equality operator in the if statement so the function will return "Equal" when val is strictly equal to 7
    Passed
    testStrict(10) should return "Not Equal"
    Passed
    testStrict(7) should return "Equal"
    Passed
    testStrict("7") should return "Not Equal"
    Passed
    You should use the === operator
    
    JavaScript básico: comparação com o operador Strict Equality
    
    A igualdade estrita (===) é a contrapartida do operador de igualdade (==). No entanto, ao contrário do operador de igualdade, que tenta converter os dois valores sendo comparados a um tipo comum, o operador de igualdade estrita não executa uma conversão de tipo.
    
    Se os valores que estão sendo comparados tiverem tipos diferentes, eles serão considerados desiguais e o operador de igualdade estrita retornará false.
    
    Exemplos
    
         3 === 3 // verdadeiro
         3 === '3' // false
    
    No segundo exemplo, 3 é um tipo Number e '3' é um tipo String.
    
    Use o operador de igualdade estrito na instrução if para que a função retorne "Equal" quando val for estritamente igual a 7
    Passado
    testStrict (10) deve retornar "não igual"
    Passado
    testStrict (7) deve retornar "igual"
    Passado
    testStrict ("7") deve retornar "não igual"
    Passado
    Você deve usar o operador ===

---
+++ JavaScript/Comparison with the Strict Equality Operator.js
@@ -0,0 +1,22 @@
+//setup
+function testStrict(val) {
+	if (val === '10') {
+	// Change this line
+	return "Equal";
+	}
+	return "Not Equal";
+}
+
+//Change this value to test
+testStrict(10);
+
+function testStrict(val) {
+	if (val === 7) {
+	// Change this line
+	return "Eqaul";
+	}
+	return "Not Equal";
+}
+
+// Change this value to test
+testStrict(7);
