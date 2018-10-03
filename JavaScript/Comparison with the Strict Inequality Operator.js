089506561c34ff13b9378942a3d3775ae2af1f43
Robson Alves <robsonmie@hotmail.com>
2018-10-02 23:13:55 -0300

    Comparison with the Strict Inequality Operator
    Basic JavaScript: Comparison with the Strict Inequality Operator
    
    The strict inequality operator (!==) is the logical opposite of the strict equality operator. It means "Strictly Not Equal" and returns false where strict equality would return true and vice versa. Strict inequality will not convert data types.
    
    Examples
    
        3 !== 3 // false
        3 !== '3' // true
        4 !== 3 // true
    
    Add the strict inequality operator to the if statement so the function will return "Not Equal" when val is not strictly equal to 17
    testStrictNotEqual(17) should return "Equal"
    testStrictNotEqual("17") should return "Not Equal"
    testStrictNotEqual(12) should return "Not Equal"
    testStrictNotEqual("bob") should return "Not Equal"
    You should use the !== operator
    
    JavaScript básico: comparação com o operador Strict Inequality
    
    O operador de desigualdade restrita (! ==) é o oposto lógico do operador de igualdade estrito. Significa "estritamente não igual" e retorna falso onde a igualdade estrita retornaria verdadeira e vice-versa. A desigualdade estrita não converte tipos de dados.
    
    Exemplos
    
         3! == 3 // false
         3! == '3' // true
         4! == 3 // verdadeiro
    
    Adicione o operador de desigualdade restrita à instrução if para que a função retorne "Not Equal" quando val não for estritamente igual a 17
    testStrictNotEqual (17) deve retornar "Igual"
    testStrictNotEqual ("17") deve retornar "não igual"
    testStrictNotEqual (12) deve retornar "não igual"
    testStrictNotEqual ("bob") deve retornar "não igual"
    Você deve usar o operador!

---
+++ JavaScript/Comparison with the Strict Inequality Operator.js
@@ -0,0 +1,15 @@
+// setup 
+function testStrictNotEqual(val) {
+    // Only Change Code Below this line
+
+    if (val !== 17) {
+
+    // Only Change Code Above this line
+    (val !== '17')
+     return "Not Equal";    
+    }
+    return "Equal";
+}
+
+// Change this value to test  
+testStrictNotEqual(10);
