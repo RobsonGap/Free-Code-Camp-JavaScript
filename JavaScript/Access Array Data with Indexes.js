e153fcaec7ba43801232ea6c052e3976c6f5751e
Robson Alves <robsonmie@hotmail.com>
2019-01-01 23:39:24 -0200

    Update Access Array Data with Indexes.js

--- JavaScript/Access Array Data with Indexes.js
+++ JavaScript/Access Array Data with Indexes.js
@@ -2,9 +2,9 @@
 var ourArray = [50,60,70];
 var ourData = ourArray[0]; // equals 50
 
-// Setup
+// Setup maquina
 var myArray = [50,60,70];
 
-// Only change code below this line.
+//Devo mudar essa linhas
 var myData = [60,70,80];
 var myData = myArray[0]; //equals 70
